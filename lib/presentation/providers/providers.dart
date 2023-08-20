6ed012981e5f7246215c3062a28fd7d1499f1d16
Pablo Perez <42857634+tripleP16@users.noreply.github.com>
2023-08-19 23:08:24 -0400

    compass done

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -3,3 +3,4 @@ export 'permissions/permissions_provider.dart';
 export 'sensors/gyroscope_provider.dart';
 export 'sensors/accelometers_provider.dart';
 export 'sensors/magnetometer_provider.dart';
+export 'compass/compass_provider.dart';
--- lib/presentation/screens/sensors/compass_screen/compass_screen.dart
+++ lib/presentation/screens/sensors/compass_screen/compass_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:second_app/presentation/providers/permissions/permissions_provider.dart';
+import 'dart:math' show pi;
+import 'package:second_app/presentation/providers/providers.dart';
 import 'package:second_app/presentation/screens/screens.dart';
 
 class CompassScreen extends ConsumerWidget {
@@ -14,6 +15,8 @@ class CompassScreen extends ConsumerWidget {
       return const AskLocationScreen();
     }
 
+    final compassHeading$ = ref.watch(compassProvider);
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -24,13 +27,53 @@ class CompassScreen extends ConsumerWidget {
         backgroundColor: Colors.black,
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: const Center(child: Compass()),
+      body: Center(
+          child: compassHeading$.when(
+              data: (value) => Compass(heading: value ?? 0),
+              error: (error, stackTrace) => Text(
+                    '$error',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+              loading: () => const CircularProgressIndicator(
+                    backgroundColor: Colors.white,
+                  ))),
     );
   }
 }
 
-class Compass extends StatelessWidget {
-  const Compass({super.key});
+class Compass extends StatefulWidget {
+  final double heading;
+  const Compass({super.key, required this.heading});
+
+  @override
+  State<Compass> createState() => _CompassState();
+}
+
+class _CompassState extends State<Compass> {
+    double prevValue = 0.0;
+  double turns = 0;
+
+  double getTurns() {
+
+    double? direction = widget.heading;
+    direction = (direction < 0) ? (360 + direction): direction;
+
+    double diff = direction - prevValue;
+    if(diff.abs() > 180) {
+
+      if(prevValue > direction) {
+        diff = 360 - (direction-prevValue).abs();
+      } else {
+        diff = 360 - (prevValue-direction).abs();
+        diff = diff * -1;
+      }
+    }
+
+    turns += (diff / 360);
+    prevValue = direction;
+
+    return turns * -1;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,18 +81,25 @@ class Compass extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        const Text('155', 
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30
-          )
+        // Text(widget.heading.toStringAsFixed(2),
+        //     style: const TextStyle(color: Colors.white, fontSize: 30)),
+        const SizedBox(
+          height: 10,
         ),
-        const SizedBox(height: 10,),
         Stack(
           alignment: Alignment.center,
           children: [
             Image.asset('assets/images/compass/quadrant-1.png'),
-            Image.asset('assets/images/compass/needle-2.png')
+            // Transform.rotate(
+            //     angle: (heading * (pi / 180) * -1),
+            //     child: ),
+
+            AnimatedRotation(
+              turns: getTurns(), 
+              duration: const Duration(seconds: 1),
+              curve: Curves.easeInOut,
+              child: Image.asset('assets/images/compass/needle-2.png'),
+            )
           ],
         )
       ],
