8b9c98b5c2e4af7852d1cf84c3b3ac56a3d69807
Pablo Perez <pperez@wbinnova.com>
2023-12-17 17:24:49 +0100

    isaar

--- lib/domain/entities/pokemon.dart
+++ lib/domain/entities/pokemon.dart
@@ -1,10 +1,12 @@
+import 'package:isar/isar.dart';
+part 'pokemon.g.dart';
+
+@collection
 class Pokemon {
+  final Id isarId = Isar.autoIncrement;
   final int id;
   final String name;
   final String sprinteFront;
 
-  Pokemon({
-    required this.id,
-    required this.name,
-    required this.sprinteFront});
+  Pokemon({required this.id, required this.name, required this.sprinteFront});
 }
